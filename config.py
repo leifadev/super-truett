5f1b9259f05b25251905940f9f83b54eeaa302da
vanillahow <49649192+leifadev@users.noreply.github.com>
2022-04-30 22:22:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT = {
-    'TOKEN': 'OODExNDI3NTQ0NzAzNTY1ODY0.YCyCyg.eoIGwufbRbuqxKCd2An7gV5N-To',
+    'TOKEN': '...',
     'PREFIX': '!',
     'MODE': 'TEST',  # I use different value here depending on if dev or prod
     'LOG_SERVER': 987654312987645312,
